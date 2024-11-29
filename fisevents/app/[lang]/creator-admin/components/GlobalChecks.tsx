9666cdcdd42e3cfea4bc9dac13519afc12c19d02
Christian Zanchetta <>
2024-11-29 10:16:47 +0100

    FE changed the create event mutation

--- fisevents/app/[lang]/creator-admin/components/GlobalChecks.tsx
+++ fisevents/app/[lang]/creator-admin/components/GlobalChecks.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { getDictionary } from '@/lib/i18n.utils';
 import { useNotification } from '@/components/Notification/useNotification';
+import { CreatorAdminRoutes } from '@/lib/routes';
 
 export type GlobalChecksProps = {
   dictionary: Awaited<
@@ -17,11 +18,15 @@ export default function GlobalChecks({ dictionary }: GlobalChecksProps) {
 
   useEffect(() => {
     if (session.status === 'authenticated' && !session.data?.user?.name) {
+      const redirectTo = CreatorAdminRoutes.getItem('user-account');
+
       showNotification({
         title: dictionary.action_required,
         message: dictionary.missing_user_name,
         type: 'error'
       });
+
+      window.location.href = redirectTo;
     }
   }, [dictionary, session, showNotification]);
 
