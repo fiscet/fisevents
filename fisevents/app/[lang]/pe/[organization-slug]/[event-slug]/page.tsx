52a950ba8abbe625b4c3cc0b35d96edc831a9b67
Christian Zanchetta <>
2024-12-13 11:28:18 +0100

    FE cleanings

--- fisevents/app/[lang]/pe/[organization-slug]/[event-slug]/page.tsx
+++ fisevents/app/[lang]/pe/[organization-slug]/[event-slug]/page.tsx
@@ -22,9 +22,6 @@ export default async function PublicEventPage({
 }) {
   const eventData = await getEventSingleBySlug({ slug: eventSlug });
   const userData = await getUserBySlug({ slug: organizationSlug });
-
-  console.log(eventData);
-
   const emailDictionary = await getEmailDictionary(lang);
 
   const showForm =
@@ -44,7 +41,7 @@ export default async function PublicEventPage({
           <PublicEvent eventData={eventData} userData={userData} lang={lang} />
 
           {showForm && (
-            <NotificationProvider className="mt-0 md:mt-0">
+            <NotificationProvider>
               <EventAttendantForm
                 lang={lang}
                 eventId={eventData._id!}
